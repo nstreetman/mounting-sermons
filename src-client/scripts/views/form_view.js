6f5e1f29d0942e1688f5a9abd7a9d9d643fd0f8d
Brandon Callahan <brandoncallahan@Brandons-MacBook-Pro.local>
2017-03-17 11:31:25 -0400

    avoiding merge

--- src-client/scripts/views/form_view.js
+++ src-client/scripts/views/form_view.js
@@ -1,1 +1,33 @@
-import React from 'react'
+import Backbone from 'Backbone'
+
+// const FormView = Backbone.View.extend({
+//   el:'#app-container',
+//   events : {
+//     'submit #new-sermon' : 'handleSermonSubmit'
+//   },
+//
+//   handleSermonSubmit: function(evt){
+//     evt.preventDefault()
+//     let formEl = evt.target
+//
+//     let formData= {
+//       name: "The Rev. Chris Warner"
+//       dateOfSermon:
+//       campus: "DanielIsland"
+//       series: "Formed In Christ"
+//     }
+//   },
+//   _templateHtml: function (){
+//
+//     return`
+//       <div className="form-container">
+//       <h2 className="form-header">HELLO this is where the form goes!"</h1>
+//
+//
+//
+//       </div>
+//
+//
+//     `
+//   }
+// })
