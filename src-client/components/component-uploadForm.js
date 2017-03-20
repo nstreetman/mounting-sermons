f776131a34432c84143cdedc39dfecf06ee621ea
nstreetman <nicole.streetman@Nicoles-MacBook-Air.local>
2017-03-20 16:59:21 -0400

    moments.js

--- src-client/components/component-uploadForm.js
+++ src-client/components/component-uploadForm.js
@@ -25,25 +25,39 @@ _handleSermonSubmit: function(evt){
 
 )
 }
-<form class="M-form-group M-grid">
-<div class="form-field M-sm-12-of-12 M-md-6-of-12">
+<form className="M-form-group M-grid">
+<div className="form-field M-sm-12-of-12 M-md-6-of-12">
     <label>Clergy</label>
     <select>
       <option value="Warner">'The Rev. Chris Warner'</option>
       <option value="Norris">'The Rev. Sean Norris'</option>
       <option value="Spencer">'The Rev. Trevor Spencer'</option>
       <option value="Bennett">'The Rev. Jonathan Bennett'</option>
+      <option value="AddNew">'Add New...'</option>
   </select>
 </div>
 
-   <div class="form-field M-sm-12-of-12 M-md-6-of-12">
-       <label>Series</label>
+   <div className="form-field M-sm-12-of-12 M-md-6-of-12">
+       <label>Sermon Series</label>
        <select>
          <option value="GenHearts">'Generous Hearts'</option>
          <option value="LifeTogether">'Life Together'</option>
          <option value="Formed">'Formed'</option>
          <option value="CrazyBusy">'Crazy Busy'</option>
+         <option value="AddNew">'Add New...'</option>
      </select>
+   </div>
+
+   <div className="form-field M-sm-12-of-12 M-md-6-of-12">
+       <label>Campus</label>
+       <select>
+         <option value="SI">'Generous Hearts'</option>
+         <option value="DI">'Sullivan&#8217;s Island'</option>
+         <option value="ION">'I&#8217;On'</option>
+     </select>
+   </div>
+   
+
    </div>
 
 </form>
