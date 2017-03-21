784f9f91ff5261d28be5a7562d59a63a13547fc7
cr0wbait <etcothran@gmail.com>
2017-03-21 12:18:25 -0400

    resolving render conflict

--- src-client/scripts/components/component-footer.js
+++ src-client/scripts/components/component-footer.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let footerStyle = {
-  background-color: '#333'
-  height: '15%'
-}
+// let footerStyle = {
+//   background-color: '#333',
+//   height: '15%',
+// }
 
-let buttonStyle {
-  border: '2px solid #fff'
-	background-color: '#333'
-	color: '#fff'
-}
+// let buttonStyle {
+//   border: '2px solid #fff',
+// 	background-color: '#333',
+// 	color: '#fff',
+// }
 
 export const FooterComponent = React.createClass({
    // _makeFooter: function(evt){
@@ -24,8 +24,8 @@ export const FooterComponent = React.createClass({
 
          return(
 
-           <footer style={footerStyle}>
-           <div style='text-align:center;' className='footer_container'>
+
+           <div className='footer_container'>
            <div className='container-fluid footer_content'>
            <div className='row options'>
                <div className='col-sm-4 directory'>
@@ -35,17 +35,17 @@ export const FooterComponent = React.createClass({
                  <button className='btn btn-default'>GIVE ONLINE HERE</button>
                </div>
                <div className='col-sm-4 social'>
-                 <button style='border-radius: %50;' className='btn btn-default socialbtn'><i className="fa fa-facebook" aria-hidden="true"></i></button>
-                 <button style='border-radius: %50;' className='btn btn-default socialbtn'><i className="fa fa-twitter" aria-hidden="true"></i></button>
-                 <button style='border-radius: %50;' className='btn btn-default socialbtn'><i className="fa fa-vimeo" aria-hidden="true"></i></button>
-                 <button style='border-radius: %50;' className='btn btn-default socialbtn'><i className="fa fa-flickr" aria-hidden="true"></i></button>
+                 <button className='btn btn-default socialbtn'><i className="fa fa-facebook" aria-hidden="true"></i></button>
+                 <button className='btn btn-default socialbtn'><i className="fa fa-twitter" aria-hidden="true"></i></button>
+                 <button className='btn btn-default socialbtn'><i className="fa fa-vimeo" aria-hidden="true"></i></button>
+                 <button className='btn btn-default socialbtn'><i className="fa fa-flickr" aria-hidden="true"></i></button>
                </div>
              </div>
            </div>
            <p className='directions'>Church of the Holy Cross, 2520 Middle Street, Sullivan&#39;s Island, SC 29482 843-883-3586 info@HolyCross.net</p>
            <p className='designer_tag'>Designed by eCothran, bCallahan &#38; nStreetman &#169; 2017 | Powered by Coffee</p>
            </div>
-           </footer>
+
 
          )
       }
