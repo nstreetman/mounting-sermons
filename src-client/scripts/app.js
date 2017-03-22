2760bd9dc4feb2f1afefc32019e5e46309bd1fee
Brandon Callahan <brandoncallahan@Brandons-MacBook-Pro.local>
2017-03-22 16:57:29 -0400

    imported audio player in app.js

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Backbone from 'backbone'
+import YouTubeAudio from 'react-youtube-audio'
 
 import {ViewController} from './viewController.js'
 import {SermonCollection} from './models/model-sermon.js'
