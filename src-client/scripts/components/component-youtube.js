34a7cf86c18ef878baeba8df4d950bc0901769d9
Brandon Callahan <brandoncallahan@Brandons-MacBook-Pro.local>
2017-03-24 11:08:29 -0400

    avoiding mergre

--- src-client/scripts/components/component-youtube.js
+++ src-client/scripts/components/component-youtube.js
@@ -3,13 +3,11 @@ import YouTubeAudio from 'react-youtube-audio'
 import {ACTIONS} from '../actions'
 
 export const AudioComponent = React.createClass({
-      // _handleSermonInfo: function(currentSermonData){
-      //    let currentSermonData = {
-      //       videoID: "",
-      //       name: "",
-      //       title: "",
-      //       collection: ""
-      //    }
+   _HandlingSeriesSermon: function(evt){
+      let selectedSeries = evt.target.dataset.route
+      console.log('pros#2', selectedSeries);
+
+   },
    render: function(){
       return (
          <div classname="YouTube">
