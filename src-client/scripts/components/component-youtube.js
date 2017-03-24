9e0c9e3f850958e7fa5cf9ee28000ba169a3551f
Brandon Callahan <brandoncallahan@Brandons-MacBook-Pro.local>
2017-03-24 11:36:12 -0400

    added YC data

--- src-client/scripts/components/component-youtube.js
+++ src-client/scripts/components/component-youtube.js
@@ -5,9 +5,17 @@ import {ACTIONS} from '../actions'
 export const AudioComponent = React.createClass({
    _HandlingSeriesSermon: function(evt){
       let selectedSeries = evt.target.dataset.route
-      console.log('pros#2', selectedSeries);
 
    },
+   _handlingClergySermon: function(evt){
+      let selectedClery = evt.target.dataset.route
+
+   },
+   _handlingCampusSermon: function(evt){
+      let selectedCampus = evt.target.dataset.route
+
+   },
+   
    render: function(){
       return (
          <div classname="YouTube">
