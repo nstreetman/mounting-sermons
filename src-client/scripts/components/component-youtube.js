c994566b132d0a7d7657ed6786d9b6c8fed0ec47
Brandon Callahan <brandoncallahan@Brandons-MacBook-Pro.local>
2017-03-27 10:19:25 -0400

    Audio Component

--- src-client/scripts/components/component-youtube.js
+++ src-client/scripts/components/component-youtube.js
@@ -3,22 +3,26 @@ import YouTubeAudio from 'react-youtube-audio'
 import {ACTIONS} from '../actions'
 
 export const AudioComponent = React.createClass({
-   _HandlingSeriesSermon: function(evt){
-      let selectedSeries = evt.target.dataset.route
-
+   getInitialState: function(){
+      return{
+         series: 'SERIES'
+      }
    },
-   _handlingClergySermon: function(evt){
-      let selectedClery = evt.target.dataset.route
-
+   _handleNewSeries: function(evt){
+      evt.preventDefault();
+      let sermonEL = evt.target
+      let sermonElObj = {
+         campus: sermonEl.sermonElObj.campus,
+         clergy: sermonEl.sermonElObj.clergy,
+         series: sermonEl.sermonElObj.series
+      }
+      ACTIONS.saveNewSermon(sermonElObj)
    },
-   _handlingCampusSermon: function(evt){
-      let selectedCampus = evt.target.dataset.route
 
-   },
 
    render: function(){
       return (
-         <div classname="YouTube">
+         <div className="YouTube">
             <YouTubeAudio/>
          </div>
       )
