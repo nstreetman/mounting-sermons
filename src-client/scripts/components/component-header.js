6e8ac5d404a150f9382a0196e1e3eb6414fe0a59
cr0wbait <etcothran@gmail.com>
2017-03-29 15:57:02 -0400

    styling wiring

--- src-client/scripts/components/component-header.js
+++ src-client/scripts/components/component-header.js
@@ -6,13 +6,14 @@ export const HeaderComponent = React.createClass({
    render: function(){
       return(
          <div className="header top-menu flex-container">
-            <div className="top-menu flex-item"><a href="http://www.holycross.net/">HOME</a></div>
-            <div className="top-menu flex-item"><a href="http://www.holycross.net/whoweare/">ABOUT</a></div>
-            <div className="top-menu flex-item"><a href="http://www.holycross.net/worship/">WORSHIP</a></div>
+            <div className="top-menu flex-item mo_bars"><i className="fa fa-bars" aria-hidden="true"></i></div>
+            <div className="top-menu flex-item navlink"><a href="http://www.holycross.net/">HOME</a></div>
+            <div className="top-menu flex-item navlink"><a href="http://www.holycross.net/whoweare/">ABOUT</a></div>
+            <div className="top-menu flex-item navlink"><a href="http://www.holycross.net/worship/">WORSHIP</a></div>
             <div className="top-menu flex-item logo"><img src="../images/HClogo.png"/></div>
-            <div className="top-menu flex-item"><a href="http://www.holycross.net/contact/">CONTACT</a></div>
-            <div className="top-menu flex-item"><a href="http://www.holycross.net/events/">EVENTS</a></div>
-            <div className="top-menu flex-item"><a href="http://www.holycross.net/howdoi/">HOW DO I...</a></div>
+            <div className="top-menu flex-item navlink"><a href="http://www.holycross.net/contact/">CONTACT</a></div>
+            <div className="top-menu flex-item navlink"><a href="http://www.holycross.net/events/">EVENTS</a></div>
+            <div className="top-menu flex-item navlink"><a href="http://www.holycross.net/howdoi/">HOW DO I...</a></div>
          </div>
       )
    }
