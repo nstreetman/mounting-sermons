3b18bcfc93d84cddda82b2ff1f41fafb1c891735
nstreetman <nicole.streetman@nicoles-air.attlocal.net>
2017-03-28 22:42:29 -0400

    Misc

--- src-client/scripts/components/component-header.js
+++ src-client/scripts/components/component-header.js
@@ -5,14 +5,14 @@ import ReactDOM from 'react-dom'
 export const HeaderComponent = React.createClass({
    render: function(){
       return(
-         <div className="header container-fluid">
-            <div className="row top-menu"><a href="http://www.holycross.net/">HOME</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/whoweare/">ABOUT</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/worship/">WORSHIP</a></div>
-            <div className="row top-menu"><img src="../images/HClogo.png"/></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/contact/">CONTACT</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/events/">EVENTS</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/howdoi/">HOW DO I...</a></div>
+         <div className="header top-menu flex-container">
+            <div className="top-menu flex-item"><a href="http://www.holycross.net/">HOME</a></div>
+            <div className="top-menu flex-item"><a href="http://www.holycross.net/whoweare/">ABOUT</a></div>
+            <div className="top-menu flex-item"><a href="http://www.holycross.net/worship/">WORSHIP</a></div>
+            <div className="top-menu flex-item logo"><img src="../images/HClogo.png"/></div>
+            <div className="top-menu flex-item"><a href="http://www.holycross.net/contact/">CONTACT</a></div>
+            <div className="top-menu flex-item"><a href="http://www.holycross.net/events/">EVENTS</a></div>
+            <div className="top-menu flex-item"><a href="http://www.holycross.net/howdoi/">HOW DO I...</a></div>
          </div>
       )
    }
