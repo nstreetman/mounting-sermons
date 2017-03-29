bf5836cd4dc394e9a468a4d86d0aa3053e2c2419
cr0wbait <etcothran@gmail.com>
2017-03-29 11:27:57 -0400

    header styling

--- src-client/scripts/components/component-header.js
+++ src-client/scripts/components/component-header.js
@@ -6,13 +6,14 @@ export const HeaderComponent = React.createClass({
    render: function(){
       return(
          <div className="header container-fluid">
-            <div className="row top-menu"><a href="http://www.holycross.net/">HOME</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/whoweare/">ABOUT</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/worship/">WORSHIP</a></div>
-            <div className="row top-menu"><img src="../images/HClogo.png"/></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/contact/">CONTACT</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/events/">EVENTS</a></div>
-            <div className="row top-menu"><a href="http://www.holycross.net/howdoi/">HOW DO I...</a></div>
+            <div className="top-menu mobile_bars"><a href="#"><i className="fa fa-bars" aria-hidden="true"></i></a></div>
+            <div className="row top-menu navlink"><a href="http://www.holycross.net/">HOME</a></div>
+            <div className="row top-menu navlink"><a href="http://www.holycross.net/whoweare/">ABOUT</a></div>
+            <div className="row top-menu navlink"><a href="http://www.holycross.net/worship/">WORSHIP</a></div>
+            <div className="row top-menu logo"><img src="../images/HClogo.png"/></div>
+            <div className="row top-menu navlink"><a href="http://www.holycross.net/contact/">CONTACT</a></div>
+            <div className="row top-menu navlink"><a href="http://www.holycross.net/events/">EVENTS</a></div>
+            <div className="row top-menu navlink"><a href="http://www.holycross.net/howdoi/">HOW DO I...</a></div>
          </div>
       )
    }
